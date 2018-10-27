504a3327f0af0dadd12ac0518e5c587895defcee
Artem Vitkovsky <artvitk@gmail.com>
2018-10-27 22:14:19 +0300

    Add controller for testing

---
+++ src/main/java/by/bsuir/demo/controller/VectorOperationsController.java
@@ -0,0 +1,14 @@
+package by.bsuir.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/operation")
+public class VectorOperationsController {
+    @GetMapping("/echo")
+    public String checkWork() {
+        return "OK!";
+    }
+}
